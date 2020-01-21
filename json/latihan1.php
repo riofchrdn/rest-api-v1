21ad510e9a845b3e86a6b6bd7c0533b99809eef2
riofchrdn <riofchrdn@gmail.com>
2020-01-21 11:51:44 +0700

    membuat koneksi dari data base ke json

--- json/latihan1.php
+++ json/latihan1.php
@@ -1,17 +1,24 @@
 <?php
 
-$mahasiswa = [
-    [
-        "nama" => "Rio Fachrudin",
-        "nrp" => "181132123212",
-        "email" => "riofchrdn@gmail.com"
-    ],
-    [
-        "nama" => "Aida Nur Fitriana",
-        "nrp" => "191132837372",
-        "email" => "aidanurfitriana@gmail.com"
-    ]
-];
+// $mahasiswa = [
+//     [
+//         "nama" => "Rio Fachrudin",
+//         "nrp" => "181132123212",
+//         "email" => "riofchrdn@gmail.com"
+//     ],
+//     [
+//         "nama" => "Aida Nur Fitriana",
+//         "nrp" => "191132837372",
+//         "email" => "aidanurfitriana@gmail.com"
+//     ]
+// ];
+
+$dbh = new PDO('mysql:host=localhost;dbname=phpdasar', 'root', '');
+
+$db = $dbh->prepare('SELECT * FROM mahasiswa');
+
+$db->execute();
+$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
 
 $data = json_encode($mahasiswa);
 echo $data;
