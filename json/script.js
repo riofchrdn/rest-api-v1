436120f6b4930239f7d7f9054f22675f5021434a
riofchrdn <riofchrdn@gmail.com>
2020-01-21 12:42:21 +0700

    merubah kondisi JSON jadi object

--- json/script.js
+++ json/script.js
@@ -1,7 +1,21 @@
-let mahasiswa = {
-    nama: "Rio Fachrudin",
-    nrp: "181132123",
-    email: "riofchrdn@gmail.com"
+//kondisi Object jadi JSON
+
+// let mahasiswa = {
+//     nama: "Rio Fachrudin",
+//     nrp: "181132123",
+//     email: "riofchrdn@gmail.com"
+// }
+// console.log(JSON.stringify(mahasiswa));
+
+
+//kondisi JSON jadi Object
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        let mahasiswa = JSON.parse(this.responseText);
+        console.log(mahasiswa);
+    }
 }
 
-console.log(JSON.stringify(mahasiswa));
+xhr.open('GET', 'coba.json', true);
+xhr.send();
