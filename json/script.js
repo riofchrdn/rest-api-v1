f897fdff9922edad34e6d41c611ae81bff758309
riofchrdn <riofchrdn@gmail.com>
2020-01-21 12:48:13 +0700

    JSON menggunakan JQuery

--- json/script.js
+++ json/script.js
@@ -9,13 +9,18 @@
 
 
 //kondisi JSON jadi Object
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        let mahasiswa = JSON.parse(this.responseText);
-        console.log(mahasiswa);
-    }
-}
+// let xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function () {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//         let mahasiswa = JSON.parse(this.responseText);
+//         console.log(mahasiswa);
+//     }
+// }
+// xhr.open('GET', 'coba.json', true);
+// xhr.send();
 
-xhr.open('GET', 'coba.json', true);
-xhr.send();
+
+//JSON menggunakan JQuery
+$.getJSON('coba.json', function (data) {
+    console.log(data);
+});
