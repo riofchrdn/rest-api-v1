ee46f9e630ae0c47b4894e951945852040eed28e
riofchrdn <riofchrdn@gmail.com>
2020-01-22 09:30:54 +0700

    membuat file script.js mengambil data JSON menggunakan JQuery

---
+++ rest-api-movie/js/script.js
@@ -0,0 +1,58 @@
+function searchmovie() {
+    $('#movie-list').html('');
+
+    $.ajax({
+        url: 'http://omdbapi.com',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            'apikey': '1bccc1f1',
+            's': $('#search-input').val()
+        },
+
+        success: function (result) {
+            if (result.Response == "True") {
+                let movies = result.Search;
+
+                $.each(movies, function (i, data) {
+                    $('#movie-list').append(`
+                    <div class="col-md-4">
+                    <div class="card mb-3">
+                        <img src="` + data.Poster + `" class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title">` + data.Title + `</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">` + data.Year + `</h6>
+                        <a href="#" class="card-link">See Detail</a>
+                        </div>
+                    </div>
+                    </div>
+                    `);
+                });
+
+                $('#search-input').val('');
+
+
+            } else {
+                $('#movie-list').html(`
+                
+                <div class="col">
+                    <h3 class="text-center">` + result.Error + `</h1>
+                </div>
+                `)
+            }
+        }
+    });
+}
+
+
+$('#search-button').on('click', function () {
+
+    searchmovie();
+
+});
+
+$('#search-input').on('keyup', function (e) {
+    if (e.keyCode === 13) {
+        searchmovie();
+    }
+});
