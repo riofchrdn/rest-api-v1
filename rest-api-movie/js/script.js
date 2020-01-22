84ff43cd79062317a1fea1d4096db54326587cf4
riofchrdn <riofchrdn@gmail.com>
2020-01-22 10:17:22 +0700

    sukses get JSON omdbAPI mengambil data lengkap movie

--- rest-api-movie/js/script.js
+++ rest-api-movie/js/script.js
@@ -83,6 +83,10 @@ $('#movie-list').on('click', '.see-detail', function () {
                         <div class="col-md-8">
                             <ul class="list-group">
                                 <li class="list-group-item"><h3>` + movie.Title + `</h3></li>
+                                <li class="list-group-item">Released :` + movie.Released + `</li>
+                                <li class="list-group-item">Genre :` + movie.Genre + `</li>
+                                <li class="list-group-item">Director :` + movie.Director + `</li>
+                                <li class="list-group-item">Actor :` + movie.Actors + `</li>
                             </ul>
                         </div>
                     </div>
